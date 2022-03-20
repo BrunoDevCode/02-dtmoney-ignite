3989af7aedd63f2cba77810513e31fb2ad8c4e4a
Bruno Henrique S <brunohenriquesantos272@gmail.com>
2022-03-20 11:48:30 -0300

    feat: adiciona todos os components

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -2,17 +2,18 @@ import { createGlobalStyle } from 'styled-components'
 
 export const GlobalStyle = createGlobalStyle`
   :root {
-    --background: #f0f2f5;
-    --red: #e52e4d;
     --blue: #5429cc;
+    --green: #33cc95;
+    --red: #e62e4d;
+
+    --background: #E5E5E5;
 
     --blue-light: #6933ff;
 
     --text-title: #363f5f;
     --text-body: #969cb3;
 
-    --background: #f0f2f5;
-    --shape: #fff;
+    --shape: #ffffff;
   }
 
   * {
