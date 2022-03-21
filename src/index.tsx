0b22bde45ea10a86deb586a663904d27aa93d58a
Bruno Henrique S <brunohenriquesantos272@gmail.com>
2022-03-20 22:15:00 -0300

    feat: adiciona modal e formulario

--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,48 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createServer } from 'miragejs';
+import { createServer, Model } from 'miragejs';
 import { App } from './App';
 
 createServer({
+  models: {
+    transaction: Model,
+  },
+
+  seeds(server: any) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Freelance de website',
+          type: 'deposit',
+          category: 'Dev',
+          amount: 6800,
+          createdAt: new Date('2022-03-14 09:30:57'),
+        },
+        {
+          id: 2,
+          title: 'Aluguel',
+          type: 'withdraw',
+          category: 'Casa',
+          amount: 1500,
+          createdAt: new Date('2022-03-05 18:04:45'),
+        },
+      ]
+    })
+  },
+
   routes() {
     this.namespace = 'api';
 
     this.get('/transactions', () => {
-      return [
-        {
-          id: 1,
-          title: 'Transaction 1',
-          amount: 400,
-          type: 'deposit',
-          category: 'Food',
-          createdAt: new Date()
-        }
-      ]
-    })
+      return this.schema.all('transaction');
+    });
+
+    this.post('/transactions', (schema: any, request: any) => {
+      const data = JSON.parse(request.requestBody);
+
+      return schema.create('transaction', data);
+    });
   }
 })
 
